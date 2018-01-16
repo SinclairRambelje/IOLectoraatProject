cf37db12f37d0731d76070afb27d2706fb9f0ea6
Sinclair Rambelje <sinne_pin@hotmail.com>
2018-01-16 22:10:40 +0100

    test
    test

--- Fontys lectoraat blog/UnitTests/UnitTest1.cs
+++ Fontys lectoraat blog/UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
@@ -6,20 +8,48 @@ namespace UnitTests
     [TestClass]
     public class UnitTest1
     {
+        Logic logic = new Logic();
         
         [TestMethod]
         public void TestMethodds1()
+
         {
-            Assert.AreEqual(1,2);
+            ConnectionHelper.Connect(AutoCreateOption.DatabaseAndSchema);
+            Session session = new Session();
+            XpoDefault.Session = session;
+
+            User user = logic.UserContext.Login("test", "test");
+            Assert.AreEqual(user.UserName, "test");
         }
 
         [TestMethod]
         public void TestMethosdsd2()
         {
-            Assert.AreEqual(1, 2);
+            Assert.AreEqual(1, 1);
         }
 
         
        
+    }
+    public static class ConnectionHelper
+    {
+        public const string ConnectionString = @"XpoProvider=MSSqlServer;data source=LAPTOP-2DGPJUM4\SQLEXPRESS;integrated security=SSPI;initial catalog=Fontys";
+        public static void Connect(DevExpress.Xpo.DB.AutoCreateOption autoCreateOption)
+        {
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(ConnectionString, autoCreateOption);
+            XpoDefault.Session = null;
+        }
+        public static DevExpress.Xpo.DB.IDataStore GetConnectionProvider(DevExpress.Xpo.DB.AutoCreateOption autoCreateOption)
+        {
+            return XpoDefault.GetConnectionProvider(ConnectionString, autoCreateOption);
+        }
+        public static DevExpress.Xpo.DB.IDataStore GetConnectionProvider(DevExpress.Xpo.DB.AutoCreateOption autoCreateOption, out IDisposable[] objectsToDisposeOnDisconnect)
+        {
+            return XpoDefault.GetConnectionProvider(ConnectionString, autoCreateOption, out objectsToDisposeOnDisconnect);
+        }
+        public static IDataLayer GetDataLayer(DevExpress.Xpo.DB.AutoCreateOption autoCreateOption)
+        {
+            return XpoDefault.GetDataLayer(ConnectionString, autoCreateOption);
+        }
     }
 }
