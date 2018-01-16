16789da87e26a1ee767ca92d22a84db8a81f3717
Sinclair Rambelje <sinne_pin@hotmail.com>
2018-01-16 21:46:14 +0100

    test
    test

--- Fontys lectoraat blog/UnitTests/UnitTest1.cs
+++ Fontys lectoraat blog/UnitTests/UnitTest1.cs
@@ -6,28 +6,7 @@ namespace UnitTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.AreEqual(1,1);
-        }
-
-        [TestMethod]
-        public void TestMethod2()
-        {
-            Assert.AreEqual(1, 1);
-        }
-
-        [TestMethod]
-        public void TestMethod3()
-        {
-            Assert.AreEqual(1, 1);
-        }
-
-        [TestMethod]
-        public void TestMethod4()
-        {
-            Assert.AreEqual(1, 1);
-        }
+        
+     
     }
 }
